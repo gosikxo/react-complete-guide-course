e81efa49c71f0fe853e260151d51f92794ae3a73
Gosia Brozek <gosikbrozek1997@gmail.com>
2023-04-27 17:52:18 +0100

    Add first component and style it

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import { ExpenseItem } from "./components/ExpenseItem";
+
 function App () {
   return (
     <div className="App">
-      <h2>Let's get started</h2>
+      <ExpenseItem />
     </div>
   );
 }
